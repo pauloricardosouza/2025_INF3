94a4347d276dca48a5748caa421f73f72a30a7ac
Paulo Ricardo de Souza Silva <30639809+pauloricardosouza@users.noreply.github.com>
2025-03-20 17:55:29 -0300

    Add files via upload
    Arquivos atualizados no dia 20/03/2025.

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sistema INF3</title>
+        <!-- Última versão compilada e minimizada do CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Última versão compilada de JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+        
+        <!-- Container para abrigar o Logotipo -->
+        <div class="container-fluid text-center p-3">
+            <a href="index.php" title="Retornar para a página inicial">
+                <img src="img/logotipo.png" style="width:150px;">
+            </a>
+        </div>
+
+        <!-- Barra de Navegação do Sistema -->
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="index.php">Página Inicial</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#formProduto.php">Cadastrar Produto</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#formLogin.php">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Início do Container para abrigar os conteúdos das páginas do sistema -->
+        <div class="container-fluid text-center mt-5 mb-5">
---
+++ index.php
@@ -0,0 +1,9 @@
+<!-- Inclui o cabeçalho através do comando include -->
+<?php include "header.php" ?>
+
+    <div class="container text-center">
+        <h1>lorem200</h1>
+    </div>
+
+    <!-- Inclui o rodapé através do comando include -->
+<?php include "footer.php" ?>

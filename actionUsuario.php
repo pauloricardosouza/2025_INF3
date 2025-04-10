10ad7c92a156a71cb3829e047cd43738876bfb39
Paulo Ricardo de Souza Silva <30639809+pauloricardosouza@users.noreply.github.com>
2025-04-10 17:53:29 -0300

    Add files via upload

--- actionUsuario.php
+++ actionUsuario.php
@@ -33,6 +33,25 @@
                 else{
                     //Armazena valor do formulário na variável
                     $dataNascimentoUsuario = filtrar_entrada($_POST["dataNascimentoUsuario"]);
+
+                    //Aplicar a função strlen() para verificar o comprimento da string da dataNascimentoUsuario
+                    if(strlen($dataNascimentoUsuario) == 10){
+
+                        //Aplicar a função substr() para gerar substrings para armazenar dia, mês e ano de nascimento do usuário
+                        $diaNascimentoUsuario = substr($dataNascimentoUsuario, 8, 2);
+                        $mesNascimentoUsuario = substr($dataNascimentoUsuario, 5, 2);
+                        $anoNascimentoUsuario = substr($dataNascimentoUsuario, 0, 4);
+
+                        //Aplicar a função checkdate() para verificar se trata-se de uma data válida
+                        if(!checkdate($mesNascimentoUsuario, $diaNascimentoUsuario, $anoNascimentoUsuario)){
+                            echo "<div class='alert alert-warning text-center'><strong>DATA INVÁLIDA</strong></div>";
+                            $erroPreenchimento = true;
+                        }
+                    }
+                    else{
+                        echo "<div class='alert alert-warning text-center'><strong>DATA INVÁLIDA</strong></div>";
+                        $erroPreenchimento = true;
+                    }
                 }
 
                 //Validação do campo cidadeUsuario
@@ -95,13 +114,46 @@
                     }
                 }
 
+                //Início da validação da foto do usuário
+                $diretorio    = "img/"; //Define para qual diretório as imagens serão movidas
+                $fotoUsuario  = $diretorio . basename($_FILES['fotoUsuario']['name']); //img/joaozinho.jpg
+                $tipoDaImagem = strtolower(pathinfo($fotoUsuario, PATHINFO_EXTENSION)); //Pega o tipo do arquivo em letras minúsculas
+                $erroUpload   = false; //Variável para controle do upload da foto
+
+                //Verifica se o tamanho do arquivo é DIFERENTE DE ZERO
+                if($_FILES['fotoUsuario']['size'] != 0){
+
+                    //Verifica se o tamanho do arquivo é maior do que 5 MegaBytes (MB) - Medida em Bytes
+                    if($_FILES['fotoUsuario']['size'] > 5000000){
+                        echo "<div class='alert alert-warning text-center'>A <strong>FOTO</strong> deve ter tamanho máximo de 5MB!</div>";
+                        $erroUpload = true;
+                    }
+
+                    //Verifica se a foto está nos formatos JPG, JPEG, PNG ou WEBP
+                    if($tipoDaImagem != "jpg" && $tipoDaImagem != "jpeg" && $tipoDaImagem != "png" && $tipoDaImagem != "webp"){
+                        echo "<div class='alert alert-warning text-center'>A <strong>FOTO</strong> deve estar nos formatos JPG, JPEG, PNG ou WEBP</div>";
+                        $erroUpload = true;
+                    }
+
+                    //Verifica se a imagem foi movida para o diretório IMG, utilizando a função move_uploaded_file
+                    if(!move_uploaded_file($_FILES['fotoUsuario']['tmp_name'], $fotoUsuario)){
+                        echo "<div class='alert alert-danger text-center'>Erro ao tentar mover a <strong>FOTO</strong> para o diretório $diretorio!</div>";
+                        $erroUpload = true;
+                    }
+
+                }
+                else{
+                    echo "<div class='alert alert-warning text-center'>O campo <strong>FOTO</strong> é obrigatório!</div>";
+                    $erroUpload = true;
+                }
+
                 //Se não houver erro de preenchimento, exibe alerta de sucesso e uma tabela com os dados informados
-                if(!$erroPreenchimento){
+                if(!$erroPreenchimento && !$erroUpload){
                     echo "<div class='alert alert-success text-center'><strong>Usuário</strong> cadastrado(a) com sucesso!</div>";
                     echo "
                         <div class='container mt-3'>
                             <div class='container mt-3 text-center'>
-                                Aqui será exibida a foto do usuário
+                                <img src='$fotoUsuario' style='width:150px;' title='Foto de $nomeUsuario'>
                             </div>
                             <table class='table'>
                                 <tr>
@@ -110,7 +162,7 @@
                                 </tr>
                                 <tr>
                                     <th>DATA DE NASCIMENTO</th>
-                                    <td>$dataNascimentoUsuario</td>
+                                    <td>$diaNascimentoUsuario/$mesNascimentoUsuario/$anoNascimentoUsuario</td>
                                 </tr>
                                 <tr>
                                     <th>CIDADE</th>
@@ -124,7 +176,7 @@
                                     <th>EMAIL</th>
                                     <td>$emailUsuario</td>
                                 </tr>
-                            </div>
+                            </table>
                         </div>
                     ";
                 }

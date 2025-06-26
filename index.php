c609ee9e756e9823ce0cf097a11582398defd439
Paulo Ricardo de Souza Silva <30639809+pauloricardosouza@users.noreply.github.com>
2025-06-26 17:47:01 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include "header.php" ?>
-        
+
 <div class='container mt-5 mb-5'>
 
     <?php
@@ -9,17 +9,45 @@
 
         //Variável PHP que recebe a Query para selecionar todos os campos da tabela Produtos
         $listarProdutos = "SELECT * FROM Produtos";
+
+        //Verificar se há algum parâmetro chamado filtroProduto sendo recebido por GET
+        if(isset($_GET['filtroProduto'])){
+            //Se houver valor setado no GET chamado 'filtroProduto', armazena na variável $
+            $filtroProduto = $_GET['filtroProduto'];
+
+            //Se o filtro for diferente de 'todos', concatena a filtragem
+            if($filtroProduto != 'todos'){
+                $listarProdutos = $listarProdutos . " WHERE statusProduto LIKE '$filtroProduto' ";
+            }
+
+            switch($filtroProduto){
+                case "todos" : $mensagemFiltro = "no total";
+                break;
+
+                case "disponivel" : $mensagemFiltro = "disponíveis";
+                break;
+
+                case "esgotado" : $mensagemFiltro = "esgotados";
+                break;
+            }
+
+        }
+        else{
+            $filtroProduto = "todos";
+            $mensagemFiltro = "no total";
+        }
+
         $res            = mysqli_query($conn, $listarProdutos); //Recebe true OR false com base na execução
         $totalProdutos  = mysqli_num_rows($res); //Retorna a quantidade de registros encontrados
 
         if($totalProdutos > 0){
             if($totalProdutos == 1){
                 //Se o total de produtos for igual a um, exibe mensagem no singular
-                echo "<div class='alert alert-info text-center'>Há <strong>$totalProdutos</strong> produto cadastrado!</div>";
+                echo "<div class='alert alert-info text-center'>Há <strong>$totalProdutos</strong> produto $mensagemFiltro cadastrado!</div>";
             }
             else{
                 //Se o total de produtos não for igual a um, exibe mensagem no plural
-                echo "<div class='alert alert-info text-center'>Há <strong>$totalProdutos</strong> produtos cadastrados!</div>";
+                echo "<div class='alert alert-info text-center'>Há <strong>$totalProdutos</strong> produtos $mensagemFiltro cadastrados!</div>";
             }
         }
         else{
@@ -30,9 +58,9 @@
             <form name='formFiltro' action='index.php' method='GET'>
                 <div class='form-floating mt-3'>
                     <select class='form-select' name='filtroProduto' required>
-                        <option value='todos'>Exibir todos os Produtos</option>
-                        <option value='disponivel'>Exibir apenas Produtos disponíveis</option>
-                        <option value='esgotado'>Exibir apenas Produtos esgotados</option>
+                        <option value='todos'"; if($filtroProduto == 'todos'){echo "selected";} echo">Exibir todos os Produtos</option>
+                        <option value='disponivel'"; if($filtroProduto == 'disponivel'){echo "selected";} echo">Exibir apenas Produtos disponíveis</option>
+                        <option value='esgotado'"; if($filtroProduto == 'esgotado'){echo "selected";} echo">Exibir apenas Produtos esgotados</option>
                     </select>
                     <label for='filtroProduto'>Selecione um Filtro</label>
                     <br>

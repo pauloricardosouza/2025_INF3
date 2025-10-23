0a8460f299c5ad0001ce04321b4fe69a89e2dad2
Paulo Ricardo de Souza Silva <30639809+pauloricardosouza@users.noreply.github.com>
2025-10-23 17:49:48 -0300

    Add files via upload

--- formEditarProduto.php
+++ formEditarProduto.php
@@ -7,6 +7,17 @@
         if(isset($_GET["idProduto"])){
             $idProduto = $_GET["idProduto"]; //Armazena o parâmetro em uma variável PHP
 
+            if(isset($_GET["statusEdicao"])){
+                $statusEdicao = $_GET["statusEdicao"];
+                switch($statusEdicao){
+                    case "sucesso" : echo "<div class='alert alert-success text-center'>Os dados do Produto foram alterados com sucesso!</div>";
+                    break;
+                    case "erro" : echo "<div class='alert alert-danger text-center'>Erro ao tentar editar os dados do Produto!</div>";
+                    break;
+                    default : header('location:index.php');
+                }
+            }
+
             include "conexaoBD.php"; //Inclui o arquivo de conexão com o Banco de Dados
 
             $buscarProduto = "SELECT * FROM Produtos WHERE idProduto = $idProduto"; //Seleciona os campos do produto
@@ -28,32 +39,35 @@
                         <div class='d-flex justify-content-center mb-3'>
                             <div class='row'>
                                 <div class='col-12'>
-                                    <form action='actionProduto.php' method='POST' class='was-validated' enctype='multipart/form-data'>
+                                    <form action='actionEditarProduto.php' method='POST' class='was-validated' enctype='multipart/form-data'>
                                         <div class='form-floating mb-3 mt-3'>
-                                            <input type='file' class='form-control' id='fotoProduto' name='fotoProduto' required>
-                                            <label for='fotoProduto'>Foto</label>
-                                            <div class='valid-feedback'></div>
-                                            <div class='invalid-feedback'></div>
+                                            <input type='text' class='form-control' id='idProduto' placeholder='ID' name='idProduto' value='$idProduto' readonly required>
+                                            <label for='idProduto'>ID do Produto</label>
+                                        </div>
+                                        <div class='form-group'>
+                                            <img src='$fotoProduto' title='Foto atual de $nomeProduto' style='width:100px'>
+                                            <input type='hidden' name='fotoAtual' value='$fotoProduto'>
+                                            <input type='file' class='btn btn-link' id='fotoProduto' name='fotoProduto'>
                                         </div>
                                         <div class='form-floating mb-3 mt-3'>
-                                            <input type='text' class='form-control' id='nomeProduto' placeholder='Nome' name='nomeProduto' required>
+                                            <input type='text' class='form-control' id='nomeProduto' placeholder='Nome' name='nomeProduto' value='$nomeProduto' required>
                                             <label for='nomeProduto'>Nome do Produto</label>
                                             <div class='valid-feedback'></div>
                                             <div class='invalid-feedback'></div>
                                         </div>
                                         <div class='form-floating mb-3 mt-3'>
-                                            <textarea class='form-control' id='descricaoProduto' placeholder='Informe uma breve descrição sobre o Produto' name='descricaoProduto' required></textarea>
+                                            <textarea class='form-control' id='descricaoProduto' placeholder='Informe uma breve descrição sobre o Produto' name='descricaoProduto' required>$descricaoProduto</textarea>
                                             <label for='descricaoProduto'>Descrição do Produto</label>
                                             <div class='valid-feedback'></div>
                                             <div class='invalid-feedback'></div>
                                         </div>
                                         <div class='form-floating mt-3 mb-3'>
-                                            <input type='text' class='form-control' id='valorProduto' placeholder='Valor do Produto' name='valorProduto' required>
+                                            <input type='text' class='form-control' id='valorProduto' placeholder='Valor do Produto' name='valorProduto' value='$valorProduto' required>
                                             <label for='valorProduto'>Valor do Produto (R$):</label>
                                             <div class='valid-feedback'></div>
                                             <div class='invalid-feedback'></div>
                                         </div>
-                                        <button type='submit' class='btn btn-success'>Cadastrar Produto</button>
+                                        <button type='submit' class='btn btn-success'>Salvar Alterações</button>
                                     </form>
                                 </div>
                             </div>

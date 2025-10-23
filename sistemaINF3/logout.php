1965c827fda52dee992ae8cbf0e819d07321c95e
Paulo Ricardo de Souza Silva <30639809+pauloricardosouza@users.noreply.github.com>
2025-10-23 16:23:15 -0300

    Delete sistemaINF3 directory

--- sistemaINF3/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start(); //Inicia uma sessão
-    session_unset(); //Apaga sessão
-    session_destroy(); //Destrói sessão
-
-    header('location:formLogin.php'); //Redireciona para o formulário de Login
-    exit();
-?>
